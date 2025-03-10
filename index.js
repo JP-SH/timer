e6276f8c5aded9a27fd95d6932ba8b22b375d626
Jasdeep Singh <j.singhhh917@gmail.com>
2025-03-10 12:22:27 +1100

    more intial commits

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hello')
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+  }
+}
