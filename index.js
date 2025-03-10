00315a0b8d08d6e61ee682237cb117d1626d81b7
Jasdeep Singh <j.singhhh917@gmail.com>
2025-03-10 16:58:15 +1100

    on start/tick/complete

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 class Timer {
-  constructor(durationInput, startButton, pauseButton) {
+  constructor(durationInput, startButton, pauseButton, callBacks) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+    if (callBacks) {
+      this.onStart = callBacks.onStart;
+      this.onTick = callBacks.onTick;
+      this.onComplete = callBacks.onComplete;
+    }
 
     this.startButton.addEventListener('click', this.start);
     this.pauseButton.addEventListener('click', this.pause);
 
   }
   start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
     this.tick();
     this.interval = setInterval(this.tick, 1000);
   };
@@ -16,9 +24,18 @@ class Timer {
     clearInterval(this.interval);
   };
   tick = () => {
-    const timeRemaining = this.timeRemaining;
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+    this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
+    }
     // the reason we dont have to use parens() when calling timeRemaining is because we used 'get' when we made the function. Get allows you to treat it as an instance variable. Get automatically invokes it.
-    this.timeRemaining = timeRemaining - 1;
   };
 
   get timeRemaining() {
@@ -33,4 +50,14 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart(){
+
+  },
+  onTick(){
+    console.log('Timer just ticked down');
+  },
+  onComplete(){
+    console.log('Timer is completed');
+  }
+});
