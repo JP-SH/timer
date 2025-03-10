550806a1ce83ae9535c741fc35c49f0a2a9d3d5d
Jasdeep Singh <j.singhhh917@gmail.com>
2025-03-10 12:40:02 +1100

    initial setup

--- index.js
+++ index.js
@@ -3,5 +3,25 @@ class Timer {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener('click', this.start);
+  }
+  start() {
+    console.log('the timer has begun');
+  }
+  pause() {
+
+  }
+  onDurationChange() {
+
+  }
+  tick() {
+
   }
 }
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
